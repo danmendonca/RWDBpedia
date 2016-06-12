f6331b60f38eec70b299fb72722f80152990ed6f
JoãoAlmeida <alm3id4@gmail.com>
2016-06-12 16:21:53 +0100

    working on the new homepage

---
+++ ResearchersDBpedia/public/scripts/controllers/homeController.js
@@ -0,0 +1,51 @@
+﻿var app = angular.module('homeModule', [])
+.controller('homeController', function ($scope, $http) {
+	
+	$scope.searchByBookButton = false;
+	$scope.searchByAuthorButton = false;
+	$scope.searchByPublisherButton = false;
+	
+	$scope.searchByBook = function () {
+		if (!$scope.searchByBookButton) return;
+		
+		// TODO resto da função:
+		var apiCall = "/authors/name/" + $scope.nameInputQuery;
+		
+		$http.get(apiCall)
+		.success(function (data) {
+			authorsBuffer = data;
+			$scope.authors = authorsBuffer;
+			$scope.IsAuthorsSingleView = false;
+		}).error(function () { console.log("Oops: could not get any data"); });
+	}
+
+	$scope.searchByAuthor = function () {
+		if (!$scope.searchByAuthorButton) return;
+		
+		// TODO resto da função:
+		var apiCall = "/authors/name/" + $scope.nameInputQuery;
+		
+		$http.get(apiCall)
+		.success(function (data) {
+			authorsBuffer = data;
+			$scope.authors = authorsBuffer;
+			$scope.IsAuthorsSingleView = false;
+		}).error(function () { console.log("Oops: could not get any data"); });
+	}
+
+	$scope.searchByPublisher = function () {
+		if (!$scope.searchByPublisherButton) return;
+		
+		// TODO resto da função:
+		var apiCall = "/authors/name/" + $scope.nameInputQuery;
+		
+		$http.get(apiCall)
+		.success(function (data) {
+			authorsBuffer = data;
+			$scope.authors = authorsBuffer;
+			$scope.IsAuthorsSingleView = false;
+		}).error(function () { console.log("Oops: could not get any data"); });
+	}
+	
+
+});
---
+++ ResearchersDBpedia/public/scripts/directives/HomeDirective.js
@@ -0,0 +1,8 @@
+﻿var app = angular.module('HomeDirective', []);
+
+app.directive('dbphome', function () {
+	return {
+		templateUrl: 'views/home.html'
+	};
+
+});
